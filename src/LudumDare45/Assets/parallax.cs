625954da8893e09c52342d11886f285d28c87f89
Orpartlu <Marvin.Somma@web.de>
2019-10-06 19:24:39 +0200

    solved the parallax problem with LWRP

---
+++ src/LudumDare45/Assets/parallax.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    public Transform myCamera;
+    public float speedCoefficient;
+    Vector3 lastPos;
+
+    void Update()
+    {
+        transform.position -= ((lastPos - myCamera.position) * speedCoefficient);
+        lastPos = myCamera.position;
+    }
+}
