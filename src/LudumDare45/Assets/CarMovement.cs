9a0d02447060d920a141d6e90231f4997097a0f4
3graba <3graba@informatik.uni-hamburg.de>
2019-10-06 15:48:31 +0200

    Car spawner and mover

---
+++ src/LudumDare45/Assets/CarMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public bool canMove = false;
+    public float speed = 5f;
+    public bool moveRight = true;
+    public Vector3 endPoint;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (!canMove)
+            return;
+
+        //transform.position = Vector3.Lerp(transform.position, endPoint, Time.deltaTime * speed);
+        transform.position = Vector3.MoveTowards(transform.position, endPoint, Time.deltaTime * speed);
+        if (Vector3.Distance(transform.position, endPoint) <= 0.5f)
+        {
+           Destroy(this.gameObject);
+        }
+    }
+}
---
+++ src/LudumDare45/Assets/CarSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject[] cars;
+    public Transform endPoint;
+    public bool stopSpawn;
+
+    public float delayMin;
+    public float delayMax;
+
+    private bool carMoveRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("spawnCar", delayMin);
+
+        carMoveRight = transform.position.x < endPoint.position.x;
+    }
+
+    void spawnCar()
+    {
+        if (stopSpawn)
+            return;
+
+        GameObject carPre = cars[Random.Range(0, cars.Length)];
+        Debug.Log("Spawn Car " + carPre.name);
+        GameObject obj = Instantiate(carPre);
+        obj.transform.position = transform.position;
+        CarMovement movement = obj.GetComponent<CarMovement>();
+        if(movement != null)
+        {
+            movement.endPoint = endPoint.position;
+            movement.moveRight = carMoveRight;
+            movement.canMove = true ;
+        }
+
+        Invoke("spawnCar", Random.Range(delayMin,delayMax));
+    }
+}
