96a94dc59c41e3dc84427719bea5b2a37f1271d4
3graba <3graba@informatik.uni-hamburg.de>
2019-10-07 07:56:56 +0200

    Sleeping spott time skip

--- src/LudumDare45/Assets/DayNightCycle.cs
+++ src/LudumDare45/Assets/DayNightCycle.cs
@@ -1,126 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DayNightCycle : MonoBehaviour
 {
+    public static DayNightCycle Instance;
+
     public Animator animator;
     public float normalizedTime = 0;
-    public float timeDay = 60;
-    public float timeNight = 60;
-    public float timeSunset = 30;
-    public float timeSunrise = 30;
+    public float dayTime = 60;
     public float gameTimer = 0;
     public bool pauseTime;
+    public string realTimeDisplay;
 
     public enum DayTime { sunrise, noon, sunset, midnight }
+    public int dayTimeCnt = Enum.GetValues(typeof(DayTime)).Length;
     public DayTime curDayTime;
 
     public delegate void OnDayTimeChange();
     public OnDayTimeChange onDayTimeChangeCallback;
 
+    private float speed = 1;
 
-    private float timerAnimation;
-    private float onDayTime = 0;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     // Use this for initialization
 
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
-        onDayTime = timeDay + timeNight + timeSunset + timeSunrise;
-        setGameTimer(gameTimer);
+        //setGameTimer(gameTimer);
     }
-    private float test = 0f;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            Debug.Log(test);
-            animator.SetFloat("gameTime", test);
-            test = test + 0.1f;
-        }
-
         if (pauseTime)
             return;
 
-        timerAnimation = timerAnimation + Time.deltaTime;
-        gameTimer = (gameTimer + Time.deltaTime) % onDayTime;
+        gameTimer = (gameTimer + Time.deltaTime* speed) % dayTime;
+        normalizedTime = Mathf.Clamp01(gameTimer / dayTime);
 
-        switch (curDayTime)
-        {
-            case DayTime.sunrise:
-                setAnimation(timeSunrise);
-                checkDayTimeChange(timeSunrise,DayTime.noon);
-                break;
-            case DayTime.noon:
-                break;
-            case DayTime.sunset:
-                break;
-            case DayTime.midnight:
-                setAnimation(timeSunset);
-                break;
-            default:
-                Debug.Log("Fail!");
-                break;
-        }
-    }
-
-    public void setGameTimer(float timer)
-    {
-        if (timer > onDayTime)
-            timer = 0;
-        gameTimer = timer;
-
-        //Check witch time of the day
-        float sunR = 0 + timeSunrise;
-        float noo = sunR + timeDay;
-        float sunS = noo + timeSunset;
-        float mid = sunS + timeNight;
-
-        Debug.Log("Set Light to: " + gameTimer);
-
-        if (gameTimer <= sunR)
-        {
-            curDayTime = DayTime.sunrise;
-            timerAnimation = sunR - gameTimer;
-            setAnimation(timeSunrise);
-        }else if (gameTimer <= noo){
-            curDayTime = DayTime.noon;
-            timerAnimation = noo - gameTimer;
-            normalizedTime = 1;
-            animator.SetFloat("gameTime", 0.5f);
-        }else if(gameTimer <= sunS)
-        {
-            curDayTime = DayTime.sunset;
-            timerAnimation = sunS - gameTimer;
-            setAnimation(timeSunset);
-        }else if (gameTimer <= mid)
-        {
-            curDayTime = DayTime.midnight;
-            timerAnimation = mid - gameTimer;
-            normalizedTime = 0;
-            animator.SetFloat("gameTime", 0f);
-        }
-    }
-
-
-    private void checkDayTimeChange(float maxTime, DayTime nextTime)
-    {
-        if (timerAnimation >= maxTime)
-        {
-            timerAnimation = 0;
-            curDayTime = nextTime;
-            if (onDayTimeChangeCallback != null)
-                onDayTimeChangeCallback.Invoke();
-        }
-
-    }
-
-    private void setAnimation(float maxTime)
-    {
-        normalizedTime = Mathf.Clamp01(timerAnimation / maxTime);
         animator.SetFloat("gameTime", normalizedTime);
+        calcRealTimeDisplay();
+        calcTimeDisplay();
+
     }
+
+    public void fastForward()
+    {
+        speed = 5;
+    }
+    public void normalTime()
+    {
+        speed = 1;
+    }
+
+    void calcRealTimeDisplay()
+    {
+        float realTime = (1440 * normalizedTime);
+        float hours = Mathf.Floor(realTime / 60);
+        float minutes = Mathf.RoundToInt(realTime % 60);
+
+        if (hours < 10)
+        {
+            realTimeDisplay = "0" + hours.ToString() + ":";
+        }
+        else
+        {
+            realTimeDisplay = hours.ToString() + ":";
+        }
+        if (minutes < 10)
+        {
+            realTimeDisplay += "0" + Mathf.RoundToInt(minutes).ToString();
+        }
+        else
+        {
+            realTimeDisplay += Mathf.RoundToInt(minutes).ToString();
+        }
+
+    }
+
+    void calcTimeDisplay()
+    {
+        DayTime newTime = curDayTime;
+        if(normalizedTime < 0.16f)
+        {
+            newTime = DayTime.sunrise;
+        }else if (normalizedTime < 0.5f)
+        {
+            newTime = DayTime.noon;
+        }else if (normalizedTime < 0.66f)
+        {
+            newTime = DayTime.sunset;
+        }else{
+            newTime = DayTime.midnight;
+        }
+
+        if(!newTime.Equals(curDayTime))
+        {
+            curDayTime = newTime; 
+            if (onDayTimeChangeCallback != null)
+            {
+                onDayTimeChangeCallback.Invoke();
+            }
+        }
+    }
+
+    
 
 }
